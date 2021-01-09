c73b5aaee4434b82e55bb402e670e89c65a8ee54
HITESHGOEL08 <hiteshgoel426@gmail.com>
2021-01-09 18:53:38 +0530

    workflow changes

--- src/screens/mainApp/App.tsx
+++ src/screens/mainApp/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload. Hola Hitesh
+          Edit <code>src/App.tsx</code> and save to reload. Hola Hitesh!!!!!!!!!
         </p>
         <a
           className="App-link"
