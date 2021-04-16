6bb1bab949a956453567623efdf750b012a04fc3
Hitesh <hitesh.goel.15csc@bml.edu.in>
2021-04-16 14:45:35 +0530

    changes

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import { HashRouter as Router, Route, Switch } from "react-router-dom"
 import App from "../screens/mainApp/App"
 import TempApp from "../screens/tempApp/TempApp"
 import "./Routes.scss"
