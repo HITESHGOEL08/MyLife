e0c84b7648a81f725b71e62f8dd518b56398cd59
Hitesh <hitesh.goel.15csc@bml.edu.in>
2022-04-02 19:39:36 +0530

    code updated

--- src/services/googleAnalyticsService/index.tsx
+++ src/services/googleAnalyticsService/index.tsx
@@ -7,8 +7,8 @@ export class googleAnalyticsService {
 
         ReactGA.initialize("UA-175785716-1", {
             gaOptions: {
-                userId: `${Math.random().toString(36).slice(2)}`,
-                name: `${Math.random().toString(36).slice(2)}`
+                userId: `${window.crypto.getRandomValues(new Uint32Array(1))[0]}`,
+                name: `user-${window.crypto.getRandomValues(new Uint32Array(1))[0]}`
             },
             testMode: false
         })
